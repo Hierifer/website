c359b12d4737f9ebde08e8c735bf0127059a4cf6
Hierifer <hierifer@hotmail.com>
2022-02-03 02:19:48 +0800

    fix: add types for build issue

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { NextPage } from 'next';
 import matter from 'gray-matter';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -24,10 +25,11 @@ export async function getStaticProps() {
   };
 }
 
-const Page: NextPage = ({ posts }) => {
+const Page: NextPage = (props: any) => {
+  const { posts } = props
     return (
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0'>
-        {posts.map(({ slug, frontmatter }) => (
+        {posts.map(({ slug, frontmatter }: { slug: string, frontmatter: any}) => (
           <div
             key={slug}
             className='border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col'
