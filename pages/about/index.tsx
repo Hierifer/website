11be828950ba6a15602c95d32d9b6ada1d769c5c
Hierifer <hierifer@hotmail.com>
2022-02-05 12:51:05 +0800

    feat: fill project and about part

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,20 +1,27 @@
 import { NextPage } from 'next'
 import { Divider } from '@arco-design/web-react';
+import { Card, Avatar, Typography } from '@arco-design/web-react';
 
 const Home: NextPage = () => {
     return (
       <div>
-        <div className="text-xl mb-4 font-bold leading-6">
-          关于 NEO-HEX 研究所
+        <div className="text-xl my-6 font-bold leading-6">
+          关于 NEO-HEX
         </div>
 
         <div className="font-light leading-8">
-          NEO-HEX 研究所是由一群爱好开源和分享的小伙伴们组成的兴趣小组。我们致力于为生活创造一些乐趣和改变。其内容形式包括（但不限于）博客文章，项目代码分享，游戏 Demo。通过
-          这些形式我们会讨论「自然语言处理」，「计算机图形学」，「产品设计」等多领域的知识和想法，也会讨论一些人生哲学的问题和小说。总而言之，这是一个内容繁杂的内容平台。希望
-          「有缘人」们能在这里激起有趣的想法，并和我们交流。
+          NEO-HEX 是由一群爱好创造和分享的人组成的兴趣小组。我们致力于为生活创造一些乐趣和改变，会分享一些博客文章，项目代码分享，游戏 Demo。通过
+          这些方式，我们会讨论各种各样的话题。欢迎大家和我们交流。
         </div>
 
         <Divider />
+
+        <Card
+          hoverable
+          className='card-with-icon-hover w-full max-w-xs'
+        >
+          <Avatar />
+        </Card>
       </div>
     )
   }
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,37 +1,61 @@
 import { NextPage } from 'next'
 const { Meta } = Card;
-import { Card } from '@arco-design/web-react';
+import { Card, Avatar, Typography } from '@arco-design/web-react';
+import { IconThumbUp, IconShareInternal, IconMore } from '@arco-design/web-react/icon';
 
-const BasicLayout: NextPage = () => {
+const Project: NextPage = () => {
     return (
-        <Card
-            hoverable
-            style={{ width: 360 }}
-            cover={
-            <div
-                style={{
-                height: 204,
-                overflow: 'hidden',
-                }}
-            >
-                <img
-                style={{ width: '100%', transform: 'translateY(-20px)' }}
-                alt='dessert'
-                src='//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/3ee5f13fb09879ecb5185e440cef6eb9.png~tplv-uwbnlip3yd-webp.webp'
-                />
-            </div>
+        <div className='grid grid-cols-3 gap-y-5'>
+            {
+                [1,2,3,4,5].map((index) => {
+                    return (
+                    <Card
+                    hoverable
+                    className='card-with-icon-hover w-full max-w-xs'
+                    cover={
+                    <div
+                        style={{
+                        height: 204,
+                        overflow: 'hidden',
+                        }}
+                    >
+                        <img
+                        style={{ width: '100%', transform: 'translateY(-20px)' }}
+                        alt='dessert'
+                        src='//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a20012a2d4d5b9db43dfc6a01fe508c0.png~tplv-uwbnlip3yd-webp.webp'
+                        />
+                    </div>
+                    }
+                    actions={[
+                    <span className='icon-hover'>
+                        <IconThumbUp />
+                    </span>,
+                    <span className='icon-hover'>
+                        <IconShareInternal />
+                    </span>,
+                    <span className='icon-hover'>
+                        <IconMore />
+                    </span>,
+                    ]}
+                    >
+                        <Meta
+                        avatar={
+                            <div style={{ display: 'flex', alignItems: 'center', color: '#1D2129' }}>
+                            <Avatar size={24} style={{ marginRight: 8 }}>
+                                A
+                            </Avatar>
+                            <Typography.Text>Username</Typography.Text>
+                            </div>
+                        }
+                        title='Card Title'
+                        description='This is the description'
+                        />
+                    </Card>
+                    )
+                })
             }
-        >
-        <Meta
-            title='Card Title'
-            description={
-                <>
-                Card content <br /> Card content
-                </>
-            }
-        />
-    </Card>
+        </div>
     )
 }
   
-export default BasicLayout
+export default Project
