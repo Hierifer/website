ed6caaa2122bfaf0aba6794dc2e95ea3b76923d4
Hierifer <hierifer@hotmail.com>
2022-02-11 01:58:12 +0800

    fix: animation effectiency problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,34 +6,43 @@ const Home: NextPage = () => {
   return (
     <div className="w-screen h-screen overflow-hidden absolute top-0 left-0">
       <div className="absolute flex flex-col top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-4xl z-20">
-        <div className="mb-4">因为热爱</div>
-        <div className="font-light mb-2">Because of Love</div>
+        <div className="mb-4 tracking-wider text-gray-800">创意源于热爱</div>
+        <div className="font-light mb-4">Inspiration from Love</div>
         <div className="flex" style={{marginLeft: '2px'}}>
-          <div className='bg-sky-300 w-3 rounded-sm h-3 mr-2' />
+          <div className='bg-blue-300 w-3 rounded-sm h-3 mr-2' />
           <div className='bg-orange-300 w-3 rounded-sm h-3 mr-2' />
           <div className='bg-red-300 w-3 rounded-sm h-3' />
         </div>
       </div>
       <div className="whole h-full" id="map" style={{width:'200%', display: 'flex'}}>
-        <World className="w-full world" />
-        <World className="w-full world" />
+        <World className="w-full world pl-2" />
+        <World className="w-full world pl-2" />
       </div>
 
       <style jsx>{`
         @keyframes example {
-          0%   {left: -100%}
-          100% {left: 0%}
+          0%   {transform: translateX(-50%)}
+          100% {transform: translateX(0%)}
         }
 
         .whole {
           animation-name: example;
-          animation-duration: 60s;
+          animation-duration: 30s;
           animation-timing-function: linear;
           animation-iteration-count: infinite;
           position: absolute;
         }
-
-
+        #map:after {
+          content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          background: white;
+          z-index: 10;
+          width:100%;
+          height: 100%;
+          opacity: 0.7;
+        }
         .hoveritem {
           position: absolute;
           background: black;
