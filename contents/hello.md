6b768cca260e12ee1db664fec0366a412ebbcfca
Hierifer <hierifer@hotmail.com>
2022-02-18 03:04:55 +0800

    feat: add content

--- contents/hello.md
+++ contents/hello.md
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
 ### Hello World
+=======
+---
+title: 欢迎来到 NEO-HEX 
+slug: hello
+uid: hh
+ctime: 1644080337543
+---
+>>>>>>> Stashed changes
 
-## Hello, asd
+NEO-HEX 是一个共享型技术 blog 网站。用于分享和展示一些有趣的想法。作者们是一些有技术背景的小伙伴。我们尝试为社区生产一些有趣的 idea 或是代码。
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next'
 const { Meta } = Card;
 import { Card } from '@arco-design/web-react';
 
+<<<<<<< Updated upstream
 const BasicLayout: NextPage = () => {
     return (
         <Card
@@ -28,6 +29,91 @@ const BasicLayout: NextPage = () => {
                 <>
                 Card content <br /> Card content
                 </>
+=======
+const USERS_JSON: {[key: string]: {icon: string, name: string}} = {
+    'hh': {
+        icon: 'https://avatars.githubusercontent.com/u/10717978?s=40&v=4',
+        name: 'Hierifer Hu'
+    },
+}
+
+const PROJECTS_JSON = [
+    {
+        name: 'H5 游戏',
+        desc: '正在 build, 点击图片进入',
+        link: 'https://games.neo-hex.com',
+        creator: 'hh',
+    },
+    {
+        name: '产品设计日志',
+        desc: '',
+        creator: 'hh',
+    },
+    {
+        name: 'Element 12.5th',
+        desc: '',
+        creator: 'hh'
+    }
+]
+
+const Project: NextPage = () => {
+    return (
+        <div className='grid gap-y-5 justify-self-center' style={{gridTemplateColumns: `repeat(auto-fill, minmax(20rem, 1fr))`}}>
+            {
+                PROJECTS_JSON.map((item, index) => {
+                    return (
+                        <Card
+                            hoverable
+                            className='card-with-icon-hover w-full max-w-xs'
+                            key={index}
+                            cover={
+                                <div
+                                    style={{
+                                    height: 204,
+                                    overflow: 'hidden',
+                                    }}
+                                >
+                                    <a target="_blank" href={item.link}>
+                                        <img
+                                            style={{ width: '100%', transform: 'translateY(-20px)' }}
+                                            alt='dessert'
+                                            src='//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a20012a2d4d5b9db43dfc6a01fe508c0.png~tplv-uwbnlip3yd-webp.webp'
+                                        />
+                                    </a>
+                                </div>
+                            }
+                            // actions={[
+                            // <span className='icon-hover'>
+                            //     <IconThumbUp />
+                            // </span>,
+                            // <span className='icon-hover'>
+                            //     <IconShareInternal />
+                            // </span>,
+                            // <span className='icon-hover'>
+                            //     <IconMore />
+                            // </span>,
+                            // ]}
+                        >
+                            <div>
+                                <div className='font-bold'>
+                                    {item.name}
+                                </div>
+                                <div className="text-gray300 h-10">
+                                    {item.desc}
+                                </div>
+                                <div>
+                                    <div style={{ display: 'flex', alignItems: 'center', color: '#1D2129' }}>
+                                        <Avatar size={24} style={{ marginRight: 8 }}>
+                                        <img alt='avatar' src={USERS_JSON[item.creator].icon} />
+                                        </Avatar>
+                                        <Typography.Text>{USERS_JSON[item.creator].name}</Typography.Text>
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    )
+                })
+>>>>>>> Stashed changes
             }
         />
     </Card>
